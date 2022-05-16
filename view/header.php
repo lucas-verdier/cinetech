5ebc2dd0dad26695556c0b2ca62ae6c5ff37f1eb
lucas-verdier <lucas.verdier@laplateforme.io>
2022-05-16 12:03:52 +0200

    ajout du header

--- view/header.php
+++ view/header.php
@@ -1,3 +1,16 @@
 <header>
+<!--    <img src="../assets/cinetech-logo.png" alt="cinetech">-->
     <h1>CINETECH</h1>
+    <form>
+        <input type="searc" placeholder="Recherche">
+        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+    </form>
+    <nav>
+        <ul>
+            <li><a href="../index.php">Accueil</a></li>
+            <li><a href="movies.php">Films</a></li>
+            <li><a href="series.php">Séries</a></li>
+            <li><a href="#"><i class="fa-solid fa-user-astronaut"></i></a></li>
+        </ul>
+    </nav>
 </header>
