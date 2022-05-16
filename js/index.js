c17249bba923bacaf3c98df2f32061dd5e481de3
lucas-verdier <lucas.verdier@laplateforme.io>
2022-05-16 10:27:01 +0200

    ajout à index 'series animmées les mieux notées'

--- js/index.js
+++ js/index.js
@@ -101,7 +101,6 @@ document.addEventListener('DOMContentLoaded', function loaded() {
 
     function similar(id) {
         var article = document.querySelector('main section:nth-of-type(3) article')
-
         fetch('https://api.themoviedb.org/3/movie/'+id+'/similar?api_key='+api_key+'&language=fr-FR')
             .then(response => response.json())
             .then(data => {
@@ -128,6 +127,44 @@ document.addEventListener('DOMContentLoaded', function loaded() {
     }
     similar(335787)
 
+    function getByGenres(type, id) {
+        var article = document.querySelector('main section:nth-of-type(4) article')
+        fetch('https://api.themoviedb.org/3/'+type+'/top_rated?api_key='+api_key+'&language=fr-FR')
+            .then(response => response.json())
+            .then(data     => {
+                var item = data.results
 
+                for (let i = 0; i<item.length; i++) {
+                    item[i].genre_ids.forEach(element => {
+                        if (element == id) {
+                            console.log(id)
+                            let a            = document.createElement('a')
+                            a.href           = './view/detail.php?'+type+'='+item[i].id
+                            let img          = document.createElement('img')
+                            img.src          = 'https://image.tmdb.org/t/p/w500/'+item[i].poster_path
+                            img.alt          = item[i].name
+                            let pTitle       = document.createElement('p')
+                            pTitle.innerHTML = item[i].name
+                            let pNote        = document.createElement('p')
+                            pNote.innerHTML  = 'Note: '+item[i].vote_average
 
+                            article.appendChild(a)
+                            a.appendChild(img)
+                            a.appendChild(pTitle)
+                            a.appendChild(pNote)
+                        }
+                    })
+                }
+            })
+    }
+    getByGenres('tv', '16')
+
+    function getGenres(type) {
+        fetch(' https://api.themoviedb.org/3/genre/'+type+'/list?api_key='+api_key+'&language=en-US')
+            .then(response => response.json())
+            .then(data     => {
+                console.log(data.genres)
+            })
+    }
+    getGenres('tv')
 })
---
+++ view/header.php
@@ -0,0 +1,3 @@
+<header>
+    <h1>CINETECH</h1>
+</header>
